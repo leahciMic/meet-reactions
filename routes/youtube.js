c93e3795267d32f328ca6ee01ba8e97ff57fd6dd
Michael Leaney <mleaney@tokbox.com>
2015-08-10 16:39:19 +1000

    Cleanup

--- routes/youtube.js
+++ routes/youtube.js
@@ -5,13 +5,12 @@ var youtubeSearch = require('youtube-search');
 
 var opts = {
   maxResults: 10,
-  key: process.env.YOUTUBE_KEY 
+  key: process.env.YOUTUBE_KEY
 };
 
 router.use(bodyParser.json());
 
 router.post('/search', function(req, res) {
-  console.log(req.body);
   youtubeSearch(req.body.query, opts, function(err, results) {
     if (err) {
       return res.status(500).send(err.message);
